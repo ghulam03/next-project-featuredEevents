06934a21e8b32a7790ffb55e42fd26ca828ab927
ghulam03 <ghulam8905@gmail.com>
2023-01-07 12:46:31 +0530

    first-commit

---
+++ pages/api/newsletter.js
@@ -0,0 +1,33 @@
+import { connectDatabase, insertDocument } from '../../helpers/db-util';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes('@')) {
+      res.status(422).json({ message: 'Invalid email address.' });
+      return;
+    }
+
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
+
+    try {
+      await insertDocument(client, 'newsletter', { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!' });
+      return;
+    }
+
+    res.status(201).json({ message: 'Signed up!' });
+  }
+}
+
+export default handler;
